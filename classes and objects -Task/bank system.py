db6e8bad0d22e581d3c59453b0c033170aecfae5
Chennaiahgari Tejashwi <165808875+TejashwiC@users.noreply.github.com>
2025-09-19 14:14:55 +0530

    Update bank system.py

--- classes and objects -Task/bank system.py
+++ classes and objects -Task/bank system.py
@@ -1,39 +1,36 @@
 class BankAccount:
-    def __init__(self,account_number,account_holder,balance=0):
-        self.account_number=account_number
-        self.account_holder=account_holder
-        self.balance=balance
-    def deposit(self,amount):
-        if amount>0:
-            self.balance+=amount
-            print(f"deposited {amount}.new balance={self.balance}")
-        else:
-            print("deposit amount must be positive")
-    def withdraw(self,amount):
-        if amount>self.balance:
-            print("insuffienct balance")
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
 
-        elif amount<=0:
-            print("withdrawl amount must be positive")
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited {amount}. New balance = {self.balance}")
         else:
-            self.balance-=amount
-            print(f"withdrawal{amount}.new balance={self.balance}")
+            print("Invalid deposit amount")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance = {self.balance}")
+
     def check_balance(self):
         print(f"Current Balance for {self.account_holder}: {self.balance}")
 
     def account_info(self):
         print("Account Information")
-        print(f"Account Number: {self.account_number}")
         print(f"Account Holder: {self.account_holder}")
         print(f"Balance: {self.balance}")
-
-acc1 = BankAccount(1000, "Teja", 5000)
-
+acc1 = BankAccount("Teja", 5000)
+acc2 = BankAccount("Poojitha", 6000)
 acc1.account_info()
-
 acc1.deposit(2000)
-
-acc1.withdraw(0)
-
+acc1.withdraw(0)   
 acc1.check_balance()
-    
+acc2.account_info()
+acc2.deposit(1000)
+acc2.withdraw(300)
+acc2.check_balance()
