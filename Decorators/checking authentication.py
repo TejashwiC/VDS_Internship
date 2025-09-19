1a9f48e10cceb45ff95ac14e55bbcf056e5b6131
Chennaiahgari Tejashwi <165808875+TejashwiC@users.noreply.github.com>
2025-09-19 13:41:53 +0530

    Update checking authentication.py

--- Decorators/checking authentication.py
+++ Decorators/checking authentication.py
@@ -1,27 +1,22 @@
 from functools import wraps
-import re
+
+VALID_USERNAME = "teju"
 
 def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        while True:
-            username = input("Enter username: ").strip().lower()
-            password = input("Enter password: ").strip()
+        username = VALID_USERNAME.lower().strip()
 
-            if (len(password) >= 8 and
-                re.search(r"[A-Z]", password) and
-                re.search(r"[a-z]", password) and
-                re.search(r"[0-9]", password) and
-                re.search(r"[!@#$%^&*(),.?\":{}|<>]", password)):
-
-                print(f"Login successful! ✅ Welcome, {username}\n")
-                return func(username)
-            else:
-                print("Password must be at least 8 characters and include uppercase, lowercase, number, and special character.\n")
+        if username:  
+            print(f"Login successful! Welcome, {username}\n")
+            return func(username)
+        else:
+            print("Username authentication failed.\n")
     return wrapper
 
 @login_required
 def access_dashboard(username):
     print(f"Welcome to your dashboard, {username}!\n")
+
 
 access_dashboard()
